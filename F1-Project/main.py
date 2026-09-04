9072040e4f965ec68cd9e983eff6467c9fb352e1
MatinHabi <a1888637@adelaide.edu.au>
2026-09-04 12:42:47 +0930

    initial commit

---
+++ F1-Project/main.py
@@ -0,0 +1,17 @@
+import fastf1 as f
+from fastf1 import plotting
+import matplotlib.pyplot as plt
+
+f.Cache.enable_cache('fastf1_cache')
+
+session = f.get_session(2025, 'Austria', 'R')
+session.load()
+
+tbl = session.results.drop(columns=['HeadshotUrl', 'CountryCode'])
+#print(f"\n\n\n{tbl.to_string()}")
+
+
+cols = ['TeamName','Time','Status','Points']
+print(f"\n\n\n{session.results[cols].to_string()}")
+
+
