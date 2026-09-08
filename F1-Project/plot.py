add477e0594fb30937cd518c1c644bb0d8d62a21
MatinHabi <matin.habibian04@gmail.com>
2026-09-08 15:25:18 +0930

    cleaned with Z-scores

--- F1-Project/plot.py
+++ F1-Project/plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy
 
 file = pd.read_csv('ver_austria.csv')
 
@@ -25,5 +26,23 @@ plt.ylabel("LapTime")
 
 """
 # <----- Normalized data ------ >
+plt.figure(num="Normalized")
+x = x_data
+y = y_data
+
+x_mean, x_std = x.mean(), x.std()
+y_mean, y_std = y.mean(), y.std()
+
+xn = (x - x_mean)/x_std
+yn = (y - y_mean)/y_std
+
+sns.scatterplot(data=file,x=xn,y=yn,hue=year,palette='viridis')
+
+plt.title("NORMALISED")
+plt.xlabel("LapTime")
+plt.ylabel("LapNumber")
+plt.legend(title='Year',bbox_to_anchor=(1.05,1),loc='upper right')
+
 plt.show()
 
+#< --------------- CLEANED 
