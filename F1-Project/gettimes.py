ed5557d88a3ae45d6375241529ade54815527eb4
MatinHabi <matin.habibian04@gmail.com>
2026-09-09 12:42:43 +0930

    made changes to build_dataset()

--- F1-Project/gettimes.py
+++ F1-Project/gettimes.py
@@ -16,15 +16,20 @@ def get_driver_laps(year, driver, circuit, session_type='R'):
     session.load(telemetry=False,weather=True,messages=False)
     weather = session.weather_data
 
-    driver_session = session.laps.pick_driver(driver)
+    driver_session = session.laps.pick_driver(driver) if(driver != '') else  session.laps
+
     data = pd.merge_asof(driver_session.sort_values('Time'), weather.sort_values('Time'), on='Time')
 
-    driver_laps = data[['LapNumber', 'LapTime', 'TyreLife', 'TrackTemp', 'Compound']].copy()
+    driver_laps = data[['Driver', 'Team', 'LapNumber', 'LapTime', 'Position',
+            'Stint', 'TyreLife', 'Compound', 'FreshTyre',
+            'TrackStatus', 'PitInTime', 'PitOutTime', 'IsAccurate',
+            'TrackTemp', 'AirTemp', 'Humidity', 'Rainfall']].copy()
+    
     driver_laps['Year'] = year
     driver_laps['LapTime'] = driver_laps['LapTime'].dt.total_seconds()
     return driver_laps
 
-def build_dataset(driver, circuit, session_type='R', years=range(2018, 2026)):
+def build_dataset(driver='', circuit='Melbourne', session_type='R', years=range(2018, 2026)):
     frames = []
     for y in years:
         frames.append(get_driver_laps(y,driver,circuit,session_type))
@@ -34,6 +39,14 @@ def build_dataset(driver, circuit, session_type='R', years=range(2018, 2026)):
     data_frame['Compound'] = data_frame['Compound'].replace('SUPERSOFT','SOFT')
     data_frame = add_stint(data_frame)
 
-    data_frame.to_csv(f'{driver}_{circuit}.csv')
+    #ordering based on drivers
+    res = session.result.set_index('Abbreviation')
+    data_frame['StartingPosition'] = data_frame['Driver'].map(res['GridPosition']) #mapping each driver with their starting position (which was decided before the race)
+
+    if(driver != ''):
+        data_frame.to_csv(f'{driver}_{circuit}.csv', index= False)
+    else:
+        data_frame.to_csv(f'all_{circuit}.csv', index= False)
+    
     return data_frame
     
--- F1-Project/plot.py
+++ F1-Project/plot.py
@@ -31,3 +31,32 @@ def iqr(file):
     plt.legend(title= 'Year', bbox_to_anchor=(1.05,1), loc='upper right')
 
 
+COMPOUND_ORDER   = ['SOFT', 'MEDIUM', 'INTERMEDIATE' , 'HARD']
+COMPOUND_COLOURS = {'SOFT': '#C81E1E','MEDIUM': '#E1A100','HARD': '#3B7DD8', 'INTERMEDIATE': '#2CA02C', 'WET': '#1F4E8C',}
+NUMERIC          = ['LapNumber', 'LapTime', 'TyreLife', 'TrackTemp']
+
+def race_trace(filename):
+    plt.figure(num="new shit")
+    file = pd.read_csv(f'{filename}')
+
+    g = sns.relplot(data=file, x='LapNumber', y='LapTime',
+                    hue='Compound', hue_order=COMPOUND_ORDER, palette=COMPOUND_COLOURS,
+                    style='Compound', style_order=COMPOUND_ORDER,
+                    col='Year', col_wrap=4, height=2.5, aspect=1.15,
+                    s=28, alpha=0.9, edgecolor='white', linewidth=0.4)
+
+    for year, ax in g.axes_dict.items():
+        year_data = file[file['Year'] == year]
+        for _, stint in year_data.groupby('Stint'):
+            ax.plot(stint['LapNumber'], stint['LapTime'],
+                    color='0.55', linewidth=1, alpha=0.6, zorder=0)
+        for stop in year_data.groupby('Stint')['LapNumber'].min()[1:]:
+            ax.axvline(stop - 0.5, color='0.8', linestyle='--', linewidth=1, zorder=0)
+
+    g.set_titles("{col_name}")
+    g.set_axis_labels("Lap", "Lap time (s)")
+    g.figure.suptitle("Race trace by season - dashed lines are pit stops", y=1.02)
+    sns.move_legend(g, 'center right', title='Compound', frameon=False)
+
+    plt.show()
+    return g
