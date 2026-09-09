5a2ad2ed7792dbb9ea13fdadd11857a7db6d0ef5
MatinHabi <matin.habibian04@gmail.com>
2026-09-09 17:55:52 +0930

    added laptime delta so the model doesnt have to do it later, added a short term history buffer to see if the driver is getting faster or slowe (this may help with untracked dirty laps), also added median pace to track the pace of all the cars on a specific lap, added feul load cuz why not right? starts at 1.0 and ends at 0.0 also fastF1 for some reason puts some drivers starting pos. to 0 if they're starting from the pits which my data thinks is better than pole pos (fixed that), found out about the massive Ground Effect Era changed f1 made in 2022 added that too for shits and gigs (maybe it'll help later... if not then i should uhh)

--- F1-Project/buildDataset.py
+++ F1-Project/buildDataset.py
@@ -26,15 +26,47 @@ def get_driver_laps(year, driver, circuit, session_type='R'):
     driver_laps['IsOutLap'] = driver_laps['PitOutTime'].notna()
 
     status = driver_laps['TrackStatus'].astype(str)
+    # Flags for future reference: 1 clear, 2 yellow, 4 safety car, 5 red flag, 6 VSC deployed, 7 VSC ending
     driver_laps['IsSC'] = status.str.contains('4', regex=False) # safety car
-    driver_laps['IsVSC'] = status.str.contains('5',regex=False) #Virtual Safetty car
-    
+    driver_laps['IsVSC'] = status.str.contains('6',regex=False) #Virtual Safetty car
+
     driver_laps['Year'] = year
     driver_laps['LapTime'] = driver_laps['LapTime'].dt.total_seconds()
     res = session.results.set_index('Abbreviation')
     driver_laps['StartingPosition'] = driver_laps['Driver'].map(res['GridPosition']) #mapping each driver with their starting position (which was decided before the race)
         
     return driver_laps
+
+def add_features(df):
+    df = df.sort_values(['Year','Driver','LapNumber']).copy()
+    g = df.groupby(['Year','Driver'])
+
+    # what the model is actually predicting which is changes in lap time
+    df['LapTimeDelta'] = g['LapTime'].diff()
+
+    # short-term history to sees a trends not snapshots
+    for k in (1,2,3):
+        df[f'LapTime_lag{k}'] = g['LapTime'].shift(k)
+
+    # field-relative pace: subtracts out fuel burn weather and SC
+    field_median = df.groupby(['Year','LapNumber'])['LapTime'].transform('median')
+    df['PaceVsMedian'] = df['LapTime'] - field_median
+
+    # fuel load 1.0 at lights-out, 0.0 at the flag
+    total_laps = df.groupby('Year')['LapNumber'].transform('max')
+    df['FuelLoad'] = (total_laps - df['LapNumber']) / total_laps
+
+    #ALSO MAYBE BUILD: GapToAhead -> but it needs Time or LapTimeStart to get cummilative race time 
+    #idea: tyrelife * compound???
+
+    #FastF1 puts ppl in the pit at the strt of the race as pos 0 -> which is better than pole my data thinks
+    #FIXING IT
+    df['StartingPosition'] = df['StartingPosition'].replace(0,20)
+
+    #Raw Spread varies a lot beteween 2020 and 2022 which is likely due to the groud-effect aero reset REGULATION changes in 2022
+    df['GroudEffectEra'] = df['Year'] >= 2022
+
+    return df
 
 def build_dataset(circuit='Melbourne', driver='', session_type='R', years=range(2018, 2026), name=None):
     frames = []
@@ -51,6 +83,7 @@ def build_dataset(circuit='Melbourne', driver='', session_type='R', years=range(
     data_frame = pd.concat(frames, ignore_index=True)
     data_frame = data_frame.dropna(subset = ['LapNumber', 'LapTime', 'TyreLife', 'TrackTemp', 'Compound'])
     data_frame['Compound'] = data_frame['Compound'].replace('SUPERSOFT','SOFT')
+    data_frame = add_features(data_frame)
     
     if name is not None:
         filepath = name
