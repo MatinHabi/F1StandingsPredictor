6ab6704f483475a1ef7c76c0c6d0aaacbb611024
MatinHabi <matin.habibian04@gmail.com>
2026-09-09 22:38:33 +0930

    Claude the GOAT noticed that if I key on just the Location's then I'm gonna have cooked data cuz there have been times where the track location changed (i.e. Italy has Imola and Monza) soooo this means that my dumbass would accidentally add all the Monza track times to Imola's which would genuinely fuck me so fucking hard becuase the MedianPace wouldn't be shit and a bunch of my features would begarbage for that csv. Thanks Claude.

--- F1-Project/build_all_data.py
+++ F1-Project/build_all_data.py
@@ -4,17 +4,14 @@ import fastf1 as f1
 
 f1.Cache.enable_cache('fastf1_cache')
 
-years_by_location = {}
+events_by_name = {}
 
 for year in range(2018, 2026):
     schedule = f1.get_event_schedule(year, include_testing=False)
-    locations = schedule['Location']
+    for name in schedule['EventName']:
+        if name not in events_by_name:
+            events_by_name[name] = []
+        events_by_name[name].append(year)
 
-    for loc in locations:
-        if loc not in years_by_location:
-            years_by_location[loc] = []
-        years_by_location[loc].append(year)
-
-for loc in years_by_location:
-    years = years_by_location[loc]
-    bd.build_dataset(loc, years=years)
+for name in events_by_name:
+    bd.build_dataset(name, years=events_by_name[name])
